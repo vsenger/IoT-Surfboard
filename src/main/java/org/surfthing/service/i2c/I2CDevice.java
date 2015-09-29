df7789217495b5cb80f6ee1a3b47f0f24bb673d5
unknown <vinicius@globalcode.com.br>
2015-09-28 21:14:03 -0300

    all

---
+++ src/main/java/org/surfthing/service/i2c/I2CDevice.java
@@ -0,0 +1,25 @@
+package org.surfthing.service.i2c;
+
+import org.surfthing.Device;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.Timer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.surfthing.Kernel;
+/**
+ *
+ * @author vsenger
+ */
+public abstract class I2CDevice implements Device {
+
+}
